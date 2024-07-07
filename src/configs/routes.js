39367f9a83b344a5de749b4e65ba681ddc47efeb
kyanhngo <kyanh2501@gmail.com>
2024-07-08 00:40:21 +0700

    Add Post routes

--- src/configs/routes.js
+++ src/configs/routes.js
@@ -6,6 +6,9 @@ import CustomerStats from '@/pages/Customer/CustomerStats';
 import Home from '@/pages/Home';
 import OrderDetail from '@/pages/Order/OrderDetail';
 import OrderList from '@/pages/Order/OrderList';
+import CreatePost from '@/pages/Post/CreatePost';
+import PostContentEditor from '@/pages/Post/PostContentEditor';
+import PostList from '@/pages/Post/PostList';
 import AttributeList from '@/pages/Product/AttributeList';
 import CategoryList from '@/pages/Product/CategoryList';
 import ProductDetailsEdit from '@/pages/Product/ProductDetailEdit';
@@ -62,6 +65,18 @@ const routes = [
       {
         path: '/manage/order/:id',
         component: OrderDetail,
+      },
+      {
+        path: '/manage/post',
+        component: PostList,
+      },
+      {
+        path: '/manage/post/:id',
+        component: PostContentEditor,
+      },
+      {
+        path: '/manage/post/create',
+        component: CreatePost,
       },
     ],
   },
