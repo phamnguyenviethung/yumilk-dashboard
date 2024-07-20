3179ddadbdb9d98590d04f72f4a4fa7cee04e764
Viet Hung <viethung01234@gmail.com>
2024-07-20 11:11:26 +0700

    [ADD]: hide chart for staff account

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,16 @@ import RevenueMonthLineChart from '@/features/Widget/RevenueMonthLineChart';
 import TopCustomer from '@/features/Widget/TopCustomer';
 import WeeklyChart from '@/features/Widget/WeeklyChart';
 import { Box, Center, Heading, Stack, VStack } from '@chakra-ui/react';
+import { useSelector } from 'react-redux';
+
+const ForAdmin = ({ children }) => {
+  const authState = useSelector(state => state.auth);
+
+  if (authState?.userData?.role !== 'Admin') return <></>;
+
+  return children;
+};
+
 const Home = () => {
   const { data, isLoading } = useGetOrderListQuery({
     pageSize: 10,
@@ -23,9 +33,11 @@ const Home = () => {
   return (
     <VStack w='full' gap='4'>
       <HomePageBoxStats />
-      <Box w='full'>
-        <WeeklyChart />
-      </Box>
+      <ForAdmin>
+        <Box w='full'>
+          <WeeklyChart />
+        </Box>
+      </ForAdmin>
 
       <Stack
         direction={{
@@ -38,24 +50,26 @@ const Home = () => {
         <TopCustomer />
         <BestSellerProduct />
       </Stack>
-      <Box w='full'>
-        <RevenueMonthLineChart />
-      </Box>
-      <Stack
-        direction={{
-          base: 'column',
-          lg: 'row',
-        }}
-        w='full'
-        gap='4'
-      >
-        <Box flex='1'>
-          <PaymentPieChart />
+      <ForAdmin>
+        <Box w='full'>
+          <RevenueMonthLineChart />
         </Box>
-        <Box flex='4'>
-          <QuarterChart />
-        </Box>
-      </Stack>
+        <Stack
+          direction={{
+            base: 'column',
+            lg: 'row',
+          }}
+          w='full'
+          gap='4'
+        >
+          <Box flex='1'>
+            <PaymentPieChart />
+          </Box>
+          <Box flex='4'>
+            <QuarterChart />
+          </Box>
+        </Stack>
+      </ForAdmin>
       <Box h='700px' w='full' my={6}>
         <Heading as='h4' fontSize='1.4rem' color='pink.400' my={4}>
           Đơn hàng gần đây
